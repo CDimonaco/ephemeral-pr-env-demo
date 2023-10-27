56abc7f930bb96b8cdad4fc960887c7be19b901a
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-10-27 16:46:00 +0200

    Add migrator to dockerfile

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -4,11 +4,14 @@ case "$1" in
   "server")
     exec /app/server
     ;;
+  "migrate-up")
+  exec /app/migrate -path /app/migrations -database "$DB_CONNECTION" up
+  ;;
   "seeder")
     exec /app/seeder "$2"
     ;;
   *)
-    echo "Please choose between seeder or server"
+    echo "Please choose between seeder,server or migrate-up"
     exit 1
     ;;
 esac
